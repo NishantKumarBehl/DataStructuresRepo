c87913722695a8589c7efa463ec96eb5b20f3618
Nishant kumar  Behl <nrbehl@gmail.com>
2020-09-07 20:47:42 +0530

    Added addition and search logic

--- BinarySearchTree/src/main/java/BSTMain.java
+++ BinarySearchTree/src/main/java/BSTMain.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class BSTMain {
+public class BSTMain {
+
+    public static void main(String[] args) {
+
+        BinaryTree tree = new BinaryTree();
+
+        /* Let us create following BST
+              50
+           /     \
+          30      70
+         /  \    /  \
+       20   40  60   80 */
+        tree.insert(50);
+        tree.insert(30);
+        tree.insert(20);
+        tree.insert(40);
+        tree.insert(70);
+        tree.insert(60);
+        tree.insert(80);
+
+        // print inorder traversal of the BST
+        tree.inorder();
+        int key = 70;
+       if (tree.root.key == key) {
+            System.out.println( "Node has been Found");
+        }
+        else
+            System.out.println("Node Not Found");
+    }
 }
--- BinarySearchTree/src/main/java/Node.java
+++ BinarySearchTree/src/main/java/Node.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Node {
+public class Node {
+    int key;
+    Node left, right;
+
+    public Node(int item) {
+        key = item;
+        left = right = null;
+    }
 }
